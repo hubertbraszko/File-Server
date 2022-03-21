2e3ac950d40b5d211f1d5f6a595eb56a8cc2f933
hubertbraszko <hubert.braszko@gmail.com>
2022-03-21 14:37:39 +0100

    get, add, delete implementation changed

--- File Server/task/src/server/storage/Storage.java
+++ File Server/task/src/server/storage/Storage.java
@@ -3,42 +3,42 @@ package server.storage;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 
 public class Storage {
 
-    private final List<File> storedFiles = new ArrayList<>();
-    private final int MAX_NUMBER_OF_FILES = 10;
     public boolean addFile(String fileName, String fileContent) {
-        if(!containsFileWithName(fileName)) {
-            String filePath = String.format("File Server/task/src/server/data/%s", fileName);
-            File file = new File(filePath);
-            try(FileWriter fileWriter = new FileWriter(file)) {
-                fileWriter.write(fileContent);
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-                return false;
-            }
-            return storedFiles.add(file);
+        boolean result = false;
+
+        String filePath = getRelativePathToFile(fileName);
+        File file = new File(filePath);
+        try(FileWriter fileWriter = new FileWriter(file)) {
+            fileWriter.write(fileContent);
+            result = true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            result = false;
         }
-        return false;
+
+        return result;
     }
 
-    public boolean deleteFile(String name) {
-        return storedFiles.removeIf(file -> file.getName().equals(name));
+    public boolean deleteFile(String fileName) {
+        String pathToFile = getRelativePathToFile(fileName);
+        File fileToBeDeleted = new File(pathToFile);
+        return fileToBeDeleted.delete();
     }
 
-    public Optional<File> getFile(String name) {
-        return storedFiles.stream()
-                .filter(file -> name.equals(file.getName()))
-                .findFirst();
+    public Optional<File> getFile(String fileName) {
+        String pathToFile = getRelativePathToFile(fileName);
+        File file = new File(pathToFile).isFile() ? new File(pathToFile) : null;
+
+        return Optional.ofNullable(file);
     }
 
-    private boolean containsFileWithName(String name) {
-        return storedFiles.stream()
-                .anyMatch(file -> file.getName().equals(name));
+    private String getRelativePathToFile(String fileName) {
+        String dataRelativePath = "File Server/task/src/server/data/%s";
+        return String.format(dataRelativePath, fileName);
     }
 }
